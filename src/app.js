55011084bb27850c127a0a89c627165cfb3fd3af
lordstark-p <pranjalchandel98@gmail.com>
2025-01-22 16:47:01 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
+// This will only handle GET call to /user
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Pranjal", lastName: "Chandel" });
+});
+
+// this will match all the HTTP method API calls to /test
+app.use("/user", (req, res) => {
   res.send("Hello from the server!");
 });
 
