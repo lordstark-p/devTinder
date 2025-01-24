577c58b4ad6dbb4beb473486900295f06dba4163
lordstark-p <pranjalchandel98@gmail.com>
2025-01-24 07:44:48 +0530

    get-APIs

--- src/app.js
+++ src/app.js
@@ -18,6 +18,36 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established");
