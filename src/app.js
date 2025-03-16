3544bc8ab0aafbcc4d31f2d7b4ad2b6e3e923c94
lordstark-p <pranjalchandel98@gmail.com>
2025-03-16 10:59:36 +0530

    added cors()

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ require("./config/database");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
